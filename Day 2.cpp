d46c48c22d9abb3d285092f6a2a44d702a380250
Matei0112 <mateimitrea22@gmail.com>
2026-05-10 06:38:09 +0300

    Add files via upload

---
+++ Day 2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main() {
+
+    // integer
+
+    int age = 21;
+    int year = 2023;
+    int days = 7.5;
+    std::cout<<days<<std::endl;
+
+    //double can store decimals
+
+    double price = 10.99;
+    double gpa = 3.9;
+    double temperature = 25.2;
+
+    std::cout<<price<<std::endl;
+
+    //char=single char
+
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    //booleans (true or false state)
+
+    bool student = true;
+    bool power = false;
+    bool forsale = true;
+
+    //string (object that represents a sequence of text)
+    std::string name = "Matei";
+    std::cout<<name<<std::endl;
+    std::string day = "friday";
+    std::string food = "Pizza";
+    std::string address = "Thames street";
+
+    std::cout << "Hello " << name << "!" << std::endl;
+
+    std::cout << "you are " << age << " years old" << std::endl;
+
+
+
+    //int x=5;
+    //int y=6;
+    //int sum=x+y;
+   // std::cout<<x<<std::endl;
+    //std::cout<<y<<std::endl;
+    //std::cout<<sum<<std::endl;
+
+    return 0;
+}
