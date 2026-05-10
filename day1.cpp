135bab8d34d420f2b4a740b7253af259371f7c07
Matei0112 <mateimitrea22@gmail.com>
2026-05-10 06:12:57 +0300

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,17 @@
+//
+// Created by User on 5/10/2026.
+//
+
+#include <iostream>
+
+int main () {
+    // This is a comment
+    /*
+     so basically this message will be ignored
+
+     */
+
+    std::cout << "Hello, World!" << '\n';
+    std::cout << "its really good!" << '\n';
+    return 0;
+}
